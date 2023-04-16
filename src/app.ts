44d72d69203dc8a2cb8b85bbd9f0e624d4627c36
sijibomi <gbemilesanmi@gmail.com>
2023-04-16 18:25:59 +0100

    feature: connect to db and pino-pretty debug

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,45 @@
-console.log("Hello world")
+import Express from "express";
+const app = Express();
 
 
+
+import dotenv from "dotenv"
+dotenv.config()
+const PORT = process.env.PORT || 1337
+
+import mongoose from "mongoose";
+import  logger  from "./utils/logger";
+import connectDB from "./utils/connect";
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mongoose.connection.once("open" , () => {
+    console.clear()
+    //console.log("Connected to mongodb");
+    logger.info("Connected to mongodb")
+    app.listen(PORT , () => {
+        //console.log(`App is running and listening on port ${PORT}`);
+        logger.info(`App is running and listening on port ${PORT}`)
+    })
+    
+})
