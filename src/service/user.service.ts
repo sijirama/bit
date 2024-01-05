a464e6eb7b12e4621c8ff5838094e10e52cb5e08
sijibomi <gbemilesanmi@gmail.com>
2024-01-05 03:05:34 +0100

    clear repo

--- src/service/user.service.ts
+++
@@ -1,9 +0,0 @@
-import UserModel , {UserDocument} from "../model/user.model"
-
-export default async function createUser( user:Partial<Omit<UserDocument , "createdAt" | "updatedAt">>    ){
-    try {
-       return await UserModel.create(user); 
-    } catch (error:any) {
-        throw new Error(error)
-    }
-}
