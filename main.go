1a5f6677267d2cb463dc808ddb9af7616a2ae095
sijibomi <gbemilesanmi@gmail.com>
2024-01-31 00:07:37 +0100

    init project

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+type cacheKey int
+type cacheValue interface{}
+
+type CacheObject struct {
+    key   cacheKey
+    value cacheValue
+}
+
 func main() {
 	fmt.Println("HELLO WORLD")
 }
