7dc5f3b9f82b67339b7693f9d02fc5e9151db1af
sijirama <gbemilesanmi@gmail.com>
2024-02-08 16:48:37 +0100

    lru new and add

--- main.go
+++ main.go
@@ -2,14 +2,6 @@ package main
 
 import "fmt"
 
-type cacheKey int
-type cacheValue interface{}
-
-type CacheObject struct {
-    key   cacheKey
-    value cacheValue
-}
-
 func main() {
 	fmt.Println("HELLO WORLD")
 }
